e7941720fbead70ad8b9df96300450f540ff0ffd
Charlton Shih <168577923+chewton2k@users.noreply.github.com>
2025-03-31 01:37:02 -0700

    centered footer

--- src/components/HomePageBottom.jsx
+++ src/components/HomePageBottom.jsx
@@ -1,29 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import GithubButton from './GithubButton';
-import LinkedInButton from './LinkedInButton';
-import EmailButton from './EmailButton';
 
 const HomePageBottom = () => {
   return (
     <footer className="w-full text-white py-6 mt-auto">
+      <div className="container mx-auto w-full flex flex-col items-center text-center py-5">
+        {/* Contact Section */} 
+        <div className="flex flex-wrap items-center space-x-4 md:space-x-10 py-2">
+          <a href="mailto:charltonshih645@g.ucla.edu" className="text-white font-medium text-xl hover:underline">
+            Say Hi!
+          </a>
+        </div>
 
-  <div className="container mx-auto px-6 flex flex-col items-center py-5">
-    {/* Contact Section */}
-    <div className="flex flex-wrap items-center space-x-4 md:space-x-10 py-2">
-      <a href="mailto:charltonshih645@g.ucla.edu" className="text-white font-medium text-xl hover:underline">
-        Say Hi!
-      </a>
-    </div>
-
-
-    {/* Copyright Section */}
-    <div className="text-center text-white text-sm">
-      <p> Hosted by Github Pages</p>
-      <p>&copy; {new Date().getFullYear()} Charlton Shih.</p>
-    </div>
-  </div>
-</footer>
+        {/* Copyright Section */}
+        <div className="text-white text-sm">
+          <p>Hosted by Github Pages</p>
+          <p>&copy; {new Date().getFullYear()} Charlton Shih.</p>
+        </div>
+      </div>
+    </footer>
   );
 };
 
